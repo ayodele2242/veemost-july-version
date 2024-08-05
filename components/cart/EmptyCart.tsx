d96cff46eca2684dc36006043f42f5004915f96c
Fagbemi Ayodele <fagbemiayodele48@gmail.com>
2024-08-05 10:28:20 +0100

    empty cart updated

--- components/cart/EmptyCart.tsx
+++ components/cart/EmptyCart.tsx
@@ -7,7 +7,7 @@ const EmptyCart = () => {
     <div className="flex flex-col lg:flex-row lg:mx-auto justify-center items-center gap-8 xl:mb-[8rem] mb-[4rem] xl:gap-[10rem mt-[4rem]">
                 <div className="flex flex-col lg:my-auto">
                     <div className="2xl:w-[532px] lg:w-[432px] md:w-[640] mb-9">
-                        <p className="font-SemiBold font-bold 2xl:text-[64px] text-[50px] lg:text-left text-center leading-none pb-4">There's nothing in your cart</p>
+                        <p className="font-SemiBold font-bold 2xl:text-[64px] text-[50px] lg:text-left text-center leading-none pb-4">There&apos;s nothing in your cart</p>
                         <p className='text-[16px] text-[#858586] font-normal font-GilroyRegular lg:text-left text-center'>Transform your business with our products and services.</p>
                     </div>
                     <Link href="/products" className="flex justify-center items-center w-[197px] h-[56px] bg-[#D6A912] cursor-pointer text-[#FFFFFF] 
