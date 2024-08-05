1ced27a732eb6cb9e1398a930435bc993497b673
Fagbemi Ayodele <fagbemiayodele48@gmail.com>
2024-08-05 18:26:52 +0100

    pagination updated

--- pagination/Pagination.js
+++ pagination/Pagination.js
@@ -20,7 +20,8 @@ const Pagination = (props) => {
     pageSize,
   });
 
-  if (currentPage === 0 || paginationRange.length < 2) {
+  // Ensure paginationRange is an array and has items
+  if (!Array.isArray(paginationRange) || paginationRange.length < 2) {
     return null;
   }
 
@@ -32,11 +33,10 @@ const Pagination = (props) => {
     onPageChange(currentPage - 1);
   };
 
-  let lastPage = paginationRange[paginationRange.length - 1];
+  const lastPage = paginationRange[paginationRange.length - 1];
+
   return (
-    <ul
-      className={classnames('pagination-container', { [className]: className })}
-    >
+    <ul className={classnames('pagination-container', { [className]: className })}>
       <li
         className={classnames('pagination-item', {
           disabled: currentPage === 1,
--- pagination/usePagination.js
+++ pagination/usePagination.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useMemo } from 'react';
 
 export const DOTS = '...';
@@ -15,15 +14,9 @@ export const usePagination = ({
   currentPage
 }) => {
   const paginationRange = useMemo(() => {
-    const totalPageCount = Math.ceil(totalCount / pageSize);
-
-    // Pages count is determined as siblingCount + firstPage + lastPage + currentPage + 2*DOTS
+    const totalPageCount = Math.ceil(totalCount / pageSize) || 0;
     const totalPageNumbers = siblingCount + 5;
 
-    /*
-      If the number of pages is less than the page numbers we want to show in our
-      paginationComponent, we return the range [1..totalPageCount]
-    */
     if (totalPageNumbers >= totalPageCount) {
       return range(1, totalPageCount);
     }
@@ -34,11 +27,6 @@ export const usePagination = ({
       totalPageCount
     );
 
-    /*
-      We do not want to show dots if there is only one position left 
-      after/before the left/right page count as that would lead to a change if our Pagination
-      component size which we do not want
-    */
     const shouldShowLeftDots = leftSiblingIndex > 2;
     const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
 
@@ -48,7 +36,6 @@ export const usePagination = ({
     if (!shouldShowLeftDots && shouldShowRightDots) {
       let leftItemCount = 3 + 2 * siblingCount;
       let leftRange = range(1, leftItemCount);
-
       return [...leftRange, DOTS, totalPageCount];
     }
 
@@ -65,7 +52,10 @@ export const usePagination = ({
       let middleRange = range(leftSiblingIndex, rightSiblingIndex);
       return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
     }
+
+    // Default return value
+    return [];
   }, [totalCount, pageSize, siblingCount, currentPage]);
 
-  return paginationRange;
+  return paginationRange || [];
 };
